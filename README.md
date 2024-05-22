e949cf11ef65feb80968b2cfb758a1b4dc00df0c
AlicjaDobrzeniecka <alizjonizm@gmail.com>
2024-05-22 17:21:23 +0200

    Adds code from the MoE repository with some modifications

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # multi-modal-continual-learning
+
+The core code with some modifications is taken from the following repositories:
+1) e
+2) https://github.com/Thunderbeee/ZSCL/tree/main
+
+In addition, this repository extends the code with the following components:
+1) TODO
+
+
---
+++ mtil/scripts/test/Full_Shot_order1.sh
@@ -0,0 +1,36 @@
+##!/bin/bash
+
+set -v
+set -e
+set -x
+GPU=3,4
+dataset=(Aircraft Caltech101 CIFAR100 DTD EuroSAT Flowers Food MNIST OxfordPet StanfordCars SUN397)
+lr=(5e-3 1e-3 5e-3 1e-3 1e-4 1e-3 1e-3 1e-4 1e-3 1e-3 1e-3)
+chooser=(Aircraft_autochooser Caltech101_autochooser CIFAR100_autochooser DTD_autochooser EuroSAT_autochooser Flowers_autochooser Food_autochooser MNIST_autochooser OxfordPet_autochooser StanfordCars_autochooser SUN397_autochooser)
+threshold=(655e-4 655e-4 655e-4 655e-4 655e-4 655e-4 655e-4 655e-4 655e-4 655e-4 655e-4 655e-4)
+num=22 # experts num
+
+###  only need to set your ckpt_path ###
+model_ckpt_path=ckpt/full_shot_order1_1000iters
+
+# inference
+for ((j = 0; j < 11; j++)); do
+  for ((i = 0; i < ${#dataset[@]}; i++)); do
+    dataset_cur=${dataset[j]}
+
+    CUDA_VISIBLE_DEVICES=${GPU} python -m src.main --eval-only \
+        --train-mode=adapter \
+        --eval-datasets=${dataset_cur} \
+        --load ${model_ckpt_path}/${dataset[i]}.pth \
+        --load_autochooser ${model_ckpt_path}/${chooser[i]}.pth \
+        --data-location /home/alicja/data \
+        --ffn_adapt_where AdapterDoubleEncoder \
+        --ffn_adapt \
+        --apply_moe \
+        --task_id 200 \
+        --multi_experts \
+        --experts_num ${num} \
+        --autorouter \
+        --threshold=${threshold[i]}
+    done
+done
